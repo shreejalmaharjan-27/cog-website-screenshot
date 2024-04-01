3d2e185a1364bbd523c557d95060dd064822f0a0
shreejalmaharjan-27 <shreejal@rafled.com>
2024-04-01 14:30:37 +0000

    add time to wait before taking a screenshot

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 from cog import BasePredictor, Input, Path
+import time
 
 
 class Predictor(BasePredictor):
@@ -27,9 +28,17 @@ class Predictor(BasePredictor):
             description="Height of the screenshot",
             default=1080
         ),
+        wait_until: int = Input(
+            description="Time in seconds to wait before a screenshot is taken",
+            default=0
+        )
     ) -> Path:
         self.browser.set_window_size(w, h)
         self.browser.get(url)
+        if wait_until > 0:
+            for i in range(wait_until):
+                print(f"Elapsed time: {i+1}/{wait_until} seconds")
+                time.sleep(1)
 
         # debug details
         print(f"Page title: {self.browser.title}")
